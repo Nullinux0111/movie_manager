9f1c3fdc475c8fbdecc960049e74e7d045fbe52b
PKafka0320 <aiya0422@gmail.com>
2021-11-17 16:46:46 +0900

    initialize for main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import { useState } from 'react';
-import Test from './components/Test.js';
+import Main from "./Main.js";
 
 function App() {
   return (
     <div className="App">
-      <Test/>
+      <Main/>
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Main() {
+  return (
+    <div>
+        <h1>code here</h1>
+    </div>
+  );
+}
+
+export default Main;
